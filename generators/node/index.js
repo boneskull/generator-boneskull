7c30273f1ec8f7693d7791c48024b933d9a5d40f
Christopher Hiller <boneskull@boneskull.com>
2015-07-31 12:17:35 -0700

    fixes for yeoman?

--- generators/node/index.js
+++ generators/node/index.js
@@ -1,5 +1,5 @@
 'use strict';
-var yeoman = require('yeoman-generator');
+var generators = require('yeoman-generator');
 var chalk = require('chalk');
 var yosay = require('yosay');
 var camelCase = require('lodash.camelcase');
@@ -14,7 +14,7 @@ var copy = function copy(src, dest) {
     this.props);
 };
 
-module.exports = yeoman.generators.Base.extend({
+module.exports = generators.Base.extend({
   prompting: function() {
     var done = this.async();
 
